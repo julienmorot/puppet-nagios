c6133419a20ee23c1b30f1ef0d7224a02c38a862
Julien Morot <jmorot@gmail.com>
2018-10-15 19:30:28 +0200

    Update agent configuration

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -16,6 +16,35 @@ class nagios::agent (
         notify  => Service[$service_name],
     }
 
+    File { '/etc/nagios/nrpe_local.cfg':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => "puppet:///modules/${module_name}/nrpe_local.cfg",
+        require => Package[$nrpepkg_name],
+        notify  => Service[$service_name],
+    }
+
+    File { 'check_cpu':
+        path    => '/usr/lib/nagios/plugins/check_cpu',
+        ensure  => file,
+        mode    => '755',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['nagios-plugins'],
+        source  => "puppet:///modules/${module_name}/check_cpu"
+    }
+    File { 'check_mem':
+        path    => '/usr/lib/nagios/plugins/check_mem',
+        ensure  => file,
+        mode    => '755',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['nagios-plugins'],
+        source  => "puppet:///modules/${module_name}/check_mem"
+    }
+
     Service { $service_name:
         ensure  => running,
         hasrestart   => true,
